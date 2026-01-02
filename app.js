a4d62b197b3ebc9891de0d7ab1f9a30098409372
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-01-03 04:40:23 +0600

    checkbox task_alt implemented

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const lengthValue = document.querySelector("#length-value");
 const slider = document.querySelector("#slider");
+const checkboxs = document.querySelectorAll(".checkbox");
 lengthValue.textContent = slider.value;
 
 slider.addEventListener("input", () => {
   lengthValue.textContent = slider.value;
+});
+checkboxs.forEach((checkbox) => {
+  checkbox.addEventListener("click", (event) => {
+    if (event.target.innerText === "radio_button_unchecked") {
+      event.target.innerText = "task_alt";
+      event.target.nextElementSibling.nextElementSibling.checked = true;
+    } else {
+      event.target.innerText = "radio_button_unchecked";
+      event.target.nextElementSibling.nextElementSibling.checked = false;
+    }
+  });
 });
