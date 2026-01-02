024414d4aff945bad23f279b40564cd8ed19ef5f
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-01-03 05:23:03 +0600

    random password generated fuctionality added

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const lengthValue = document.querySelector("#length-value");
 const slider = document.querySelector("#slider");
 const checkboxs = document.querySelectorAll(".checkbox");
 const includeLabels = document.querySelectorAll(".include-label");
+const btn = document.querySelector("button");
+const password = document.querySelector("#password");
+
 lengthValue.textContent = slider.value;
 
 slider.addEventListener("input", () => {
   lengthValue.textContent = slider.value;
 });
+
 checkboxs.forEach((checkbox) => {
   checkbox.addEventListener("click", (event) => {
     if (event.target.innerText === "radio_button_unchecked") {
@@ -18,6 +22,7 @@ checkboxs.forEach((checkbox) => {
     }
   });
 });
+
 includeLabels.forEach((label) => {
   label.addEventListener("click", (event) => {
     if (
@@ -29,3 +34,33 @@ includeLabels.forEach((label) => {
     }
   });
 });
+
+btn.addEventListener("click", () => {
+  let length = slider.value;
+  let upperCase = document.querySelector("#upparCase").checked;
+  let lowerCase = document.querySelector("#lowerCase").checked;
+  let symbols = document.querySelector("#symbols").checked;
+  let numbers = document.querySelector("#numbers").checked;
+  let passwordGenerated = generatePassword(
+    length,
+    upperCase,
+    lowerCase,
+    symbols,
+    numbers
+  );
+  password.value = passwordGenerated;
+});
+
+function generatePassword(length, upperCase, lowerCase, symbols, numbers) {
+  let charSet = "";
+  let str = "";
+  if (upperCase) charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (lowerCase) charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toLowerCase();
+  if (symbols) charSet += "!@#$%^&*()-<>?";
+  if (numbers) charSet += "0123456789";
+  for (i = 0; i < length; i++) {
+    str += charSet.charAt(Math.round(Math.random() * charSet.length));
+  }
+  console.log(str);
+  return str;
+}
